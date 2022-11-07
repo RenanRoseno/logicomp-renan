56f267ab09cee2f9d24a97bbd021bb45fb1e17d8
Renan Roseno <rosenorenan@gmail.com>
2022-11-06 23:50:48 -0300

    Add fifth restriction

--- project_1.py
+++ project_1.py
@@ -7,8 +7,8 @@ GT = 1
 S = 2
 
 
-file_name = 'column_bin_5a_3p'
-qt_rules = 4
+file_name = 'column_bin_3a_6p'
+qt_rules = 2
 signals = ['le', 'gt', 's']
 
 with open('../arquivos_Pacientes/' + file_name + '.csv', 'r') as ficheiro:
@@ -154,22 +154,24 @@ def fourth_restriction():
                 and_formulas.append(implies_atoms_aux)
                 implies_atoms_aux = []
         patient_id = 0
-        print('-------------------')
     return and_all(and_formulas)
 
 #
 def fifth_restriction():
-    and_formulas = []
-    implies_atoms_aux = []
+    or_formulas = []
+    or_atoms_aux = []
     patient_id = 0
-    for i in range(qt_rules):
-        for patient in patients:
-            if (patient[len(patient) - 1] == '1'):
-                patient_id += 1
+    for patient in patients:
+        if (patient[len(patient) - 1] == '1'):
+            patient_id += 1
+            for i in range(qt_rules):                 
                 #print('C_' + str(i+1) + '_'+ str(patient_id))
-                atom_cover = Atom('C_' + str(i+1) + '_' + str(patient_id))
-                # print(patient)
-                index = 0
+                or_atoms_aux.append(Atom('C_' + str(i+1) + '_' + str(patient_id)))
+            #print(print_atoms(or_atoms_aux))
+            or_atoms_aux = or_all(or_atoms_aux)
+            or_formulas.append(or_atoms_aux)
+            or_atoms_aux = []   
+    return and_all(or_formulas)
 
 #  X,a,i,le
 #  a => atributo
@@ -181,34 +183,27 @@ def fifth_restriction():
 # get_atoms_description()
 
 
-#fourth_restriction()
 
-# formula =And (
-#             And(
-#                 And(
-#                     first_restriction(),
-#                     second_restriction()
-#                 ),
-#                 And(
-#                     third_restriction(), 
-#                     fourth_restriction())
-#                 ),
-#                 fifth_restriction())
 
-# formula = And(
-#              And(
-#                 first_restriction(),
-#                 second_restriction()
-#             ),
-#             third_restriction())
+formula =And (
+            And(
+                And(
+                    first_restriction(),
+                    second_restriction()
+                ),
+                And(
+                    third_restriction(), 
+                    fourth_restriction())
+                ),
+                fifth_restriction())
 
 
 # Verifica se existe solução satisfativel
-# solution = satisfiability_checking(formula)
+solution = satisfiability_checking(formula)
 
-# if solution:
-#     print(solution)
-# else:
-#     print('Não existe um conjunto com ' + str(qt_rules) +
-#           ' regras que classifique todos os pacientes')
+if solution:
+    print(convertJson(str(solution)))
+else:
+    print('Não existe um conjunto com ' + str(qt_rules) +
+          ' regras que classifique todos os pacientes')
 #print(convertJson("{'X_PI <= 70.62_1_gt': True, 'X_GS <= 37.89_4_le': True, 'X_GS <= 37.89_4_gt': True, 'X_GS <= 57.55_3_le': True, 'X_PI <= 42.09_4_gt': True, 'X_GS <= 57.55_3_s': True, 'X_PI <= 80.61_1_le': True, 'X_PI <= 70.62_2_le': True, 'X_PI <= 42.09_4_s': True, 'X_PI <= 70.62_4_le': True, 'X_PI <= 42.09_1_s': True, 'X_PI <= 70.62_3_le': True, 'X_PI <= 42.09_3_le': True, 'X_GS <= 37.89_4_s': True, 'X_PI <= 80.61_2_le': True, 'X_PI <= 80.61_3_s': True, 'X_PI <= 80.61_4_s': True, 'X_GS <= 57.55_2_le': True, 'X_GS <= 37.89_3_gt': True, 'X_PI <= 42.09_4_le': True, 'X_PI <= 42.09_1_le': True, 'X_PI <= 42.09_3_s': True, 'X_GS <= 57.55_2_s': True, 'X_GS <= 57.55_3_gt': True, 'X_PI <= 70.62_3_s': True, 'X_PI <= 42.09_3_gt': True, 'X_GS <= 37.89_2_s': True, 'X_PI <= 70.62_3_gt': True, 'X_GS <= 57.55_4_gt': True, 'X_PI <= 80.61_1_s': True, 'X_GS <= 57.55_1_s': True, 'X_PI <= 80.61_2_s': True, 'X_GS <= 37.89_1_le': True, 'X_PI <= 70.62_2_s': True, 'X_GS <= 57.55_4_s': True, 'X_GS <= 57.55_1_gt': True, 'X_PI <= 42.09_2_gt': True, 'X_GS <= 37.89_1_s': True, 'X_PI <= 70.62_4_s': False, 'X_PI <= 80.61_4_le': True, 'X_PI <= 80.61_1_gt': True, 'X_GS <= 37.89_1_gt': True, 'X_GS <= 57.55_1_le': True, 'X_PI <= 42.09_2_s': False, 'X_PI <= 42.09_1_gt': True, 'X_GS <= 37.89_2_gt': True, 'X_PI <= 70.62_1_s': False, 'X_GS <= 57.55_2_gt': True, 'X_PI <= 80.61_3_le': True, 'X_PI <= 70.62_4_gt': True, 'X_PI <= 80.61_3_gt': True, 'X_PI <= 80.61_2_gt': True, 'X_PI <= 80.61_4_gt': True, 'X_PI <= 70.62_2_gt': True, 'X_GS <= 37.89_3_le': True, 'X_GS <= 37.89_3_s': False, 'X_GS <= 57.55_4_le': True, 'X_PI <= 42.09_2_le': True, 'X_PI <= 70.62_1_le': True, 'X_GS <= 37.89_2_le': True}"))
