98df5cb46d208653142cb95600b0d24c9b1f269d
Renan Roseno <rosenorenan@gmail.com>
2022-11-06 22:23:04 -0300

    Add fourth restriction

--- project_1.py
+++ project_1.py
@@ -8,7 +8,7 @@ S = 2
 
 
 file_name = 'column_bin_5a_3p'
-qtd_rules = 1
+qt_rules = 4
 signals = ['le', 'gt', 's']
 
 with open('../arquivos_Pacientes/' + file_name + '.csv', 'r') as ficheiro:
@@ -28,7 +28,7 @@ with open('../arquivos_Pacientes/' + file_name + '.csv', 'r') as ficheiro:
 
 def get_atoms():
     atoms = []
-    for i in range(qtd_rules):
+    for i in range(qt_rules):
         for attr in attributes:
             if attr != 'P':
                 for signal in signals:
@@ -39,10 +39,11 @@ def get_atoms():
 
 atoms = get_atoms()
 
+
 def convertJson(formula):
     formula_str = str(formula)
     return formula_str.replace("'", '"').replace('True', '"True"').replace('False', '"False"')
-    
+
 
 def print_atoms(atoms_array):
     for atom in atoms_array:
@@ -72,14 +73,18 @@ def and_all(list_formulas):
     return first_formula
 
 # Cada atributo e cada regra temos exatamente uma das tres possibilidades <=, > ou não aparece
+
+
 def first_restriction():
     return or_all(atoms)
 
 # Cada regra deve ter algum atributo aparecendo nela (filtrar todos que a formula não aparece e negar)
+
+
 def second_restriction():
     or_atoms_aux = []
     or_formulas = []
-    for i in range(qtd_rules):
+    for i in range(qt_rules):
         for atom in atoms:
             string_search = '_'+str(i+1)
             if '_s' in atom.name and string_search in atom.name:
@@ -91,12 +96,14 @@ def second_restriction():
     return and_all(or_formulas)
 
 # Para cada paciente SEM patologia e cada regra, algum atributo pode não ser aplicado a regra
+
+
 def third_restriction():
     or_formulas = []
     or_atoms_aux = []
     for patient in patients:
         if (patient[len(patient) - 1] == '0'):
-            for i in range(qtd_rules):
+            for i in range(qt_rules):
                 index = 0
                 for patology in patient:
                     if attributes[index] != 'P':
@@ -111,6 +118,59 @@ def third_restriction():
                 or_formulas.append(or_atoms_aux)
                 or_atoms_aux = []
     return and_all(or_formulas)
+
+# Para cada paciente COM patologia, cada regra e cada atributo, se o atributo do paciente não se aplicar ao da regra, então a regra não cobre esse paciente
+
+
+def fourth_restriction():
+    and_formulas = []
+    implies_atoms_aux = []
+    patient_id = 0
+    for i in range(qt_rules):
+        for patient in patients:
+            if (patient[len(patient) - 1] == '1'):
+                patient_id += 1
+                #print('C_' + str(i+1) + '_'+ str(patient_id))
+                atom_cover = Atom('C_' + str(i+1) + '_' + str(patient_id))
+                # print(patient)
+                index = 0
+                for patology in patient:
+                    if (attributes[index] != 'P'):
+                        if patology == '0':
+                            atom = Atom(
+                                'X_' + attributes[index] + '_' + str(i + 1) + '_' + signals[LE])
+                            implies_atoms_aux.append(
+                                Implies(atom, Not(atom_cover)))
+                            #print('X_' + attributes[index] + '_' + str(i + 1) + '_' + signals[LE])
+                        else:
+                            atom = Atom(
+                                'X_' + attributes[index] + '_' + str(i + 1) + '_' + signals[GT])
+                            implies_atoms_aux.append(
+                                Implies(atom, Not(atom_cover)))
+                            #print('X_' + attributes[index] + '_' + str(i + 1) + '_' + signals[GT])
+                    index += 1
+                # print(implies_atoms_aux)
+                implies_atoms_aux = and_all(implies_atoms_aux)
+                and_formulas.append(implies_atoms_aux)
+                implies_atoms_aux = []
+        patient_id = 0
+        print('-------------------')
+    return and_all(and_formulas)
+
+#
+def fifth_restriction():
+    and_formulas = []
+    implies_atoms_aux = []
+    patient_id = 0
+    for i in range(qt_rules):
+        for patient in patients:
+            if (patient[len(patient) - 1] == '1'):
+                patient_id += 1
+                #print('C_' + str(i+1) + '_'+ str(patient_id))
+                atom_cover = Atom('C_' + str(i+1) + '_' + str(patient_id))
+                # print(patient)
+                index = 0
+
 #  X,a,i,le
 #  a => atributo
 #  i => i-ésima regra 1 < i < m
@@ -119,22 +179,36 @@ def third_restriction():
 #  n => quantidade de pacientes
 
 # get_atoms_description()
-#print(first_restriction())
-# []
-#formula = []
-formula = And(
-    And(
-        first_restriction(),
-        second_restriction()
-    ),
-    third_restriction())
 
 
-# # Verifica se existe solução satisfativel
-solution = satisfiability_checking(formula)
-print(convertJson(solution))
+#fourth_restriction()
+
+# formula =And (
+#             And(
+#                 And(
+#                     first_restriction(),
+#                     second_restriction()
+#                 ),
+#                 And(
+#                     third_restriction(), 
+#                     fourth_restriction())
+#                 ),
+#                 fifth_restriction())
+
+# formula = And(
+#              And(
+#                 first_restriction(),
+#                 second_restriction()
+#             ),
+#             third_restriction())
+
+
+# Verifica se existe solução satisfativel
+# solution = satisfiability_checking(formula)
+
 # if solution:
 #     print(solution)
 # else:
-#     print('Não existe um conjunto com ' + str(qtd_rules) +
+#     print('Não existe um conjunto com ' + str(qt_rules) +
 #           ' regras que classifique todos os pacientes')
+#print(convertJson("{'X_PI <= 70.62_1_gt': True, 'X_GS <= 37.89_4_le': True, 'X_GS <= 37.89_4_gt': True, 'X_GS <= 57.55_3_le': True, 'X_PI <= 42.09_4_gt': True, 'X_GS <= 57.55_3_s': True, 'X_PI <= 80.61_1_le': True, 'X_PI <= 70.62_2_le': True, 'X_PI <= 42.09_4_s': True, 'X_PI <= 70.62_4_le': True, 'X_PI <= 42.09_1_s': True, 'X_PI <= 70.62_3_le': True, 'X_PI <= 42.09_3_le': True, 'X_GS <= 37.89_4_s': True, 'X_PI <= 80.61_2_le': True, 'X_PI <= 80.61_3_s': True, 'X_PI <= 80.61_4_s': True, 'X_GS <= 57.55_2_le': True, 'X_GS <= 37.89_3_gt': True, 'X_PI <= 42.09_4_le': True, 'X_PI <= 42.09_1_le': True, 'X_PI <= 42.09_3_s': True, 'X_GS <= 57.55_2_s': True, 'X_GS <= 57.55_3_gt': True, 'X_PI <= 70.62_3_s': True, 'X_PI <= 42.09_3_gt': True, 'X_GS <= 37.89_2_s': True, 'X_PI <= 70.62_3_gt': True, 'X_GS <= 57.55_4_gt': True, 'X_PI <= 80.61_1_s': True, 'X_GS <= 57.55_1_s': True, 'X_PI <= 80.61_2_s': True, 'X_GS <= 37.89_1_le': True, 'X_PI <= 70.62_2_s': True, 'X_GS <= 57.55_4_s': True, 'X_GS <= 57.55_1_gt': True, 'X_PI <= 42.09_2_gt': True, 'X_GS <= 37.89_1_s': True, 'X_PI <= 70.62_4_s': False, 'X_PI <= 80.61_4_le': True, 'X_PI <= 80.61_1_gt': True, 'X_GS <= 37.89_1_gt': True, 'X_GS <= 57.55_1_le': True, 'X_PI <= 42.09_2_s': False, 'X_PI <= 42.09_1_gt': True, 'X_GS <= 37.89_2_gt': True, 'X_PI <= 70.62_1_s': False, 'X_GS <= 57.55_2_gt': True, 'X_PI <= 80.61_3_le': True, 'X_PI <= 70.62_4_gt': True, 'X_PI <= 80.61_3_gt': True, 'X_PI <= 80.61_2_gt': True, 'X_PI <= 80.61_4_gt': True, 'X_PI <= 70.62_2_gt': True, 'X_GS <= 37.89_3_le': True, 'X_GS <= 37.89_3_s': False, 'X_GS <= 57.55_4_le': True, 'X_PI <= 42.09_2_le': True, 'X_PI <= 70.62_1_le': True, 'X_GS <= 37.89_2_le': True}"))
