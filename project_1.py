b72c0cf0b37cccfc229859b1719ff7003e040860
Renan Roseno <rosenorenan@gmail.com>
2022-11-07 02:18:34 -0300

    Refactoring first restriction

--- project_1.py
+++ project_1.py
@@ -7,9 +7,10 @@ GT = 1
 S = 2
 
 
-file_name = 'column_bin_3a_6p'
-qt_rules = 2
+file_name = 'column_bin_3a_5p'
+qt_rules = 1
 signals = ['le', 'gt', 's']
+array_splited = []
 
 with open('../arquivos_Pacientes/' + file_name + '.csv', 'r') as ficheiro:
     reader = csv.reader(ficheiro)
@@ -34,11 +35,16 @@ def get_atoms():
                 for signal in signals:
                     atom = Atom('X_' + attr + '_' + str(i + 1) + '_' + signal)
                     atoms.append(atom)
+    size = len(atoms)
+    unities = int(len(atoms)/3)
+    for i in range(unities):
+        start = int(i*size/unities)
+        end = int((i+1)*size/unities)
+        array_splited.append(atoms[start:end])
     return atoms
 
 
 atoms = get_atoms()
-
 
 def convertJson(formula):
     formula_str = str(formula)
@@ -76,7 +82,72 @@ def and_all(list_formulas):
 
 
 def first_restriction():
-    return or_all(atoms)
+    print('')
+    list_complete = []
+    for index in range(len(array_splited)):
+        list_a = array_splited[index]
+        list_aux = []
+        list_aux_2 = []
+        for i in range(len(list_a)):
+            for j in range(len(list_a)):
+                if i != j:
+                    list_aux.append(And(list_a[i], Not(list_a[j])))
+                    #print(type(list_a[i]))
+                    #print(list_a[i].name + " e " + "~"+list_a[j].name + " e ")
+                #list_aux.append(And(list_a[i], Not(list_a[j])))
+            #print(list_aux)
+            #list_aux_2 = and_all(list_aux)
+            #list_aux = []
+        # print(and_all(list_aux))
+            list_aux_2.append(and_all(list_aux))
+            list_aux = []
+            #print('----------------')
+        list_complete.append(or_all(list_aux_2))
+    return and_all(list_complete)
+    
+    #list_aux_2 = or_all(list_aux_2)
+    #print(list_aux_2)
+    # print('----------------')
+             #list_aux.append(And(list_a[i], Not(list_a[j])))
+        #print(or_all(list_aux))
+        #list_aux = []
+    
+    # list_atoms = []
+    # list_xor = []
+    # for i in range(qt_rules):
+    #     for attr in attributes:
+    #         if attr != 'P':
+    #             for signal in signals:
+    #                 list_atoms.app1end(Atom('X_' + attr + '_' + str(i + 1) + '_' + signal))
+    #             for i in range(len(list_atoms)):
+    #                 for j in range(len(list_atoms)-1):
+    #                     if i != j:
+    #                         list_xor.append(And(list_atoms[i], Not(list_atoms[j])))
+    #             print(and_all(list_xor))      #atoms.append(atom)
+    # splited = []
+    # qt_atoms = len(atoms)
+    # array_parts = int(qt_atoms/3)
+    # list_atoms = []
+    # for i in range(array_parts):
+    #     start = int(i*qt_atoms/array_parts)
+    #     end = int((i+1)*qt_atoms/array_parts)
+    #     splited.append(atoms[start:end])
+    # for split in splited:
+    #     for i in range(len(split)):
+    #         for j in range(len(split)) - 1:
+    #             if i != j:
+    #              list_atoms.append(And(Not(split[i]), split[j]))
+    #         print(list_atoms)
+    #     print('--------------')
+        # xor_aux = []
+        # for i in range(len(atoms)):
+        #     if i % 3 == 0:
+        #         for i_aux in range(3):
+        #             xor_aux.append(atoms[i + i_aux].name)
+        #         for atom in xor_aux:
+        #            print(atom)
+        # print(atom.name)
+        # return or_all(atoms)
 
 # Cada regra deve ter algum atributo aparecendo nela (filtrar todos que a formula não aparece e negar)
 
@@ -157,6 +228,8 @@ def fourth_restriction():
     return and_all(and_formulas)
 
 #
+
+
 def fifth_restriction():
     or_formulas = []
     or_atoms_aux = []
@@ -164,13 +237,14 @@ def fifth_restriction():
     for patient in patients:
         if (patient[len(patient) - 1] == '1'):
             patient_id += 1
-            for i in range(qt_rules):                 
+            for i in range(qt_rules):
                 #print('C_' + str(i+1) + '_'+ str(patient_id))
-                or_atoms_aux.append(Atom('C_' + str(i+1) + '_' + str(patient_id)))
-            #print(print_atoms(or_atoms_aux))
+                or_atoms_aux.append(
+                    Atom('C_' + str(i+1) + '_' + str(patient_id)))
+            # print(print_atoms(or_atoms_aux))
             or_atoms_aux = or_all(or_atoms_aux)
             or_formulas.append(or_atoms_aux)
-            or_atoms_aux = []   
+            or_atoms_aux = []
     return and_all(or_formulas)
 
 #  X,a,i,le
@@ -183,27 +257,27 @@ def fifth_restriction():
 # get_atoms_description()
 
 
+print(first_restriction())
+
+# formula =And (
+#             And(
+#                 And(
+#                     first_restriction(),
+#                     second_restriction()
+#                 ),
+#                 And(
+#                     third_restriction(),
+#                     fourth_restriction())
+#                 ),
+#                 fifth_restriction())
 
 
-formula =And (
-            And(
-                And(
-                    first_restriction(),
-                    second_restriction()
-                ),
-                And(
-                    third_restriction(), 
-                    fourth_restriction())
-                ),
-                fifth_restriction())
+# # # Verifica se existe solução satisfativel
+# solution = satisfiability_checking(formula)
 
-
-# Verifica se existe solução satisfativel
-solution = satisfiability_checking(formula)
-
-if solution:
-    print(convertJson(str(solution)))
-else:
-    print('Não existe um conjunto com ' + str(qt_rules) +
-          ' regras que classifique todos os pacientes')
+# if solution:
+#     print(convertJson(str(solution)))
+# else:
+#     print('Não existe um conjunto com ' + str(qt_rules) +
+#           ' regras que classifique todos os pacientes')
 #print(convertJson("{'X_PI <= 70.62_1_gt': True, 'X_GS <= 37.89_4_le': True, 'X_GS <= 37.89_4_gt': True, 'X_GS <= 57.55_3_le': True, 'X_PI <= 42.09_4_gt': True, 'X_GS <= 57.55_3_s': True, 'X_PI <= 80.61_1_le': True, 'X_PI <= 70.62_2_le': True, 'X_PI <= 42.09_4_s': True, 'X_PI <= 70.62_4_le': True, 'X_PI <= 42.09_1_s': True, 'X_PI <= 70.62_3_le': True, 'X_PI <= 42.09_3_le': True, 'X_GS <= 37.89_4_s': True, 'X_PI <= 80.61_2_le': True, 'X_PI <= 80.61_3_s': True, 'X_PI <= 80.61_4_s': True, 'X_GS <= 57.55_2_le': True, 'X_GS <= 37.89_3_gt': True, 'X_PI <= 42.09_4_le': True, 'X_PI <= 42.09_1_le': True, 'X_PI <= 42.09_3_s': True, 'X_GS <= 57.55_2_s': True, 'X_GS <= 57.55_3_gt': True, 'X_PI <= 70.62_3_s': True, 'X_PI <= 42.09_3_gt': True, 'X_GS <= 37.89_2_s': True, 'X_PI <= 70.62_3_gt': True, 'X_GS <= 57.55_4_gt': True, 'X_PI <= 80.61_1_s': True, 'X_GS <= 57.55_1_s': True, 'X_PI <= 80.61_2_s': True, 'X_GS <= 37.89_1_le': True, 'X_PI <= 70.62_2_s': True, 'X_GS <= 57.55_4_s': True, 'X_GS <= 57.55_1_gt': True, 'X_PI <= 42.09_2_gt': True, 'X_GS <= 37.89_1_s': True, 'X_PI <= 70.62_4_s': False, 'X_PI <= 80.61_4_le': True, 'X_PI <= 80.61_1_gt': True, 'X_GS <= 37.89_1_gt': True, 'X_GS <= 57.55_1_le': True, 'X_PI <= 42.09_2_s': False, 'X_PI <= 42.09_1_gt': True, 'X_GS <= 37.89_2_gt': True, 'X_PI <= 70.62_1_s': False, 'X_GS <= 57.55_2_gt': True, 'X_PI <= 80.61_3_le': True, 'X_PI <= 70.62_4_gt': True, 'X_PI <= 80.61_3_gt': True, 'X_PI <= 80.61_2_gt': True, 'X_PI <= 80.61_4_gt': True, 'X_PI <= 70.62_2_gt': True, 'X_GS <= 37.89_3_le': True, 'X_GS <= 37.89_3_s': False, 'X_GS <= 57.55_4_le': True, 'X_PI <= 42.09_2_le': True, 'X_PI <= 70.62_1_le': True, 'X_GS <= 37.89_2_le': True}"))
